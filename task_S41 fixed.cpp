16863ecf406d445f76c7478de4d1688dceb00705
RiddlerZero <88580858+RiddlerZero@users.noreply.github.com>
2024-10-11 20:54:01 +0800

    Add files via upload

---
+++ task_S41 fixed.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+// Задание S41 1
+// Напишите функцию squared_sum(), которая принимает два дробных числа и возвращает квадрат их суммы
+
+double squared_sum(double A, double N) {
+    double squared = (A+N)*(A+N);
+    return squared;
+}
+
+int main() {
+    double A;
+    double N;
+
+    // Ввод данных
+    cout << "Введите первое число A: ";
+    cin >> A;
+
+    cout << "Введите второе число N: ";
+    cin >> N;
+
+    double squared = squared_sum(A, N);
+
+    cout << "Первое число: " << A << endl; 
+    cout << "Второе число: " << N << endl;
+    cout << "Сумиа в квадрате= " << squared << endl;
+    
+    return 0;
+}
